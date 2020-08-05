48d93a93dfc1566847c13d64bf6abb5759d418e9
agenciaradden <contato.radden@gmail.com>
2020-08-05 19:57:32 -0300

    finalizing back end nextlevelweek

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from "express"
 
+import ClassesController from "./controller/ClassesController"
+import ConnectionController from "./controller/ConnectionController"
+
 const routes = express.Router()
 
-routes.get("/", (req, res) => {
-    res.json("recebendo usuarios")
-})
+const classesController = new ClassesController()
+const connectionsController = new ConnectionController()
+
+routes.post("/classes", classesController.create)
+
+routes.get("/classes", classesController.index)
+
+routes.get("/connections", connectionsController.index)
+
+routes.post("/connections", connectionsController.create)
 
 export default routes
